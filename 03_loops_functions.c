c8384cd8c170eae6ce876f7234b4a3d9b85bb69d
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2026-02-12 13:50:44 -0600

    03 for loop

---
+++ 03_loops_functions.c
@@ -0,0 +1,23 @@
+/*
+ * Examples:
+ * Using loops, functions
+ * 
+ * Author: Hermes Rojas Sancho
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	// ===============================
+	// FOR LOOP EXAMPLE
+	// ===============================
+	// Guide challenge solution
+	// - Write a loop that print the numbers from 1 to 10 in ascendant order
+	for (int number = 1; number <= 10; number++) {
+		printf("%d - ", number);
+	}
+	printf("\n");
+	
+	return 0;
+}

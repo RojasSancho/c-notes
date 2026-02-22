d4d0ac6a2073ddff46c8c22aa26b4fa7959b4056
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2026-02-22 13:21:05 -0600

    03 using loops exercise

---
+++ Exercises/IterativeStructures/03_using_loops_exercise.c
@@ -0,0 +1,11 @@
+/* Exercise: Using loops 
+ * Make a program that reads two integers and displays on the screen all the numbers between the given integers.
+ * 
+ * Author: Hermes Rojas Sancho
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	return 0;
+}
